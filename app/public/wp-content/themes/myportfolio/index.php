09cb8e6245f8bc0c7648e5cd15d2e86e1e0e8352
Kiyotaka <t10kiyotaka@gmail.com>
2019-08-21 22:52:29 +0900

    Modify: Split common VIEW to partial

--- app/public/wp-content/themes/myportfolio/index.php
+++ app/public/wp-content/themes/myportfolio/index.php
@@ -1,38 +1,4 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width">
-    <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-<!-- ▼ ヘッダー : 開始-->
-<header class="l-header">
-    <div class="c-header-description"><?php bloginfo('description'); ?></div>
-    <a class="c-logo" href="<?php echo esc_url( home_url('/') ); ?>">
-        <div class="c-logo__image">
-            <img src="<?php echo esc_url( get_theme_file_uri('images/logo.png') ); ?>" alt="<?php bloginfo('name'); ?>">
-        </div>
-        <div class="c-logo__text"><?php bloginfo('name'); ?></div>
-    </a>
-    <div class="c-global-nav">
-        <ul>
-            <li>
-                <a href="<?php echo esc_url( home_url('/') ); ?>">HOME</a>
-            </li>
-            <li>
-                <a href="<?php echo esc_url( home_url('/portfolio/') ); ?>">PROFILE</a>
-            </li>
-            <li>
-                <a href="<?php echo esc_url( home_url('/blog/') ); ?>">BLOG</a>
-            </li>
-            <li>
-                <a href="<?php echo esc_url( home_url('/contact/') ); ?>">CONTACT</a>
-            </li>
-        </ul>
-    </div>
-</header>
-<!-- ▲ ヘッダー : 終了-->
+<?php get_header(); ?>
 <!-- ▼ コンテンツ : 開始-->
 <div class="l-wrapper">
     <div class="l-main">
@@ -95,42 +61,17 @@
                     <?php endif; ?>
                 </div>
             </div>
-            <!-- ▼ サイドバー : 開始-->
-            <div class="l-aside">
-                <div class="c-widget">
-                    <h2 class="c-widget__title">CATEGORIES</h2>
-                    <ul>
-                        <li>
-                            <a href="#">学習記録</a>
-                        </li>
-                        <li>
-                            <a href="#">作品集</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <!--▲ サイドバー : 終了-->
+            <!-- Side bar -->
+            <?php get_sidebar(); ?>
+
         </div>
     </div>
 </div>
 <!-- ▲ コンテンツ : 終了-->
 
-<!-- ▼ フッター : 開始-->
-<footer class="l-footer">
-    <div class="l-container">
-        <p class="c-footer-description"><?php bloginfo('description'); ?></p>
-        <a class="c-logo is-white" href="../">
-            <div class="c-logo__image">
-                <img src="../images/logo.png" alt="<?php bloginfo('name'); ?>">
-            </div>
-            <div class="c-logo__text"><?php bloginfo('name'); ?></div>
-        </a>
-    </div>
-</footer>
-<div class="c-copyright">
-    <p>Copyright © 2019 <?php bloginfo('name'); ?> All Rights Reserved.</p>
-</div>
-<!-- ▲ フッター : 終了-->
+<!-- Footer -->
+<?php get_footer(); ?>
+
 <?php wp_footer(); ?>
 </body>
 </html>
